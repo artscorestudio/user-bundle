bb77e0b842276c4b03491673224a44d5d6d155c1
Nicolas Claverie <info@artscore-studio.fr>
2016-02-09 14:52:47 +0100

    Add Entity for extends FOSUserBundle entity

---
+++ Entity/User.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * This file is part of Artscore Studio Framework Package
+ *
+ * (c) 2012-2015 Artscore Studio <info@artscore-studio.fr>
+ *
+ * This source file is subject to the MIT Licence that is bundled
+ * with this source code in the file LICENSE.
+ */
+namespace ASF\UserBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+
+/**
+ * User Entity
+ * 
+ * @author Nicolas Claverie <info@artscore-studio.fr>
+ *
+ */
+class User extends BaseUser
+{
+	const ROLE_MEMBER = 'ROLE_MEMBER';
+	
+	/**
+	 * @var integer
+	 */
+	protected $id;
+}
