567b969bbe5e93f6026da909b365ebbc4f80b52e
Nicolas Claverie <info@artscore-studio.fr>
2016-02-09 14:44:46 +0100

    Initialize bundle

---
+++ ASFUserBundle.php
@@ -0,0 +1,20 @@
+<?php
+/*
+ * This file is part of the Artscore Studio Framework package.
+ *
+ * (c) 2012-2015 Nicolas Claverie <info@artscore-studio.fr>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace ASF\UserBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * User Bundle
+ * 
+ * @author Nicolas Claverie <info@artscore-studio.fr>
+ *
+ */
+class ASFUserBundle extends Bundle {}
