5c89a94dbd75a003779f625d81c4a90b04b71fd6
Nicolas Claverie <info@artscore-studio.fr>
2016-02-10 16:59:31 +0100

    Add User entity manager service

--- DependencyInjection/ASFUserExtension.php
+++ DependencyInjection/ASFUserExtension.php
@@ -9,18 +9,18 @@
  */
 namespace ASF\UserBundle\DependencyInjection;
 
-use Symfony\Component\DependencyInjection\Extension\Extension;
+use ASF\CoreBundle\DependencyInjection\ASFExtension;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use Symfony\Component\Config\FileLocator;
-use Symfony\Component\DependencyInjection\Loader;
 
 /**
  * This is the class that loads and manages your bundle configuration
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class ASFUserExtension extends Extension
+class ASFUserExtension extends ASFExtension
 {
     /**
      * {@inheritdoc}
@@ -28,8 +28,9 @@ class ASFUserExtension extends Extension
     public function load(array $configs, ContainerBuilder $container)
     {
         $configuration = new Configuration();
-        $config = $this->processConfiguration($configuration, $configs);
-
-        $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+	    $config = $this->processConfiguration($configuration, $configs);
+	    
+	    $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+	    $loader->load('services.xml');
     }
 }
