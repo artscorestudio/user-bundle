3ff688582be2e2b40db03b385ee893f2bd2f68d5
Nicolas Claverie <info@artscore-studio.fr>
2016-03-01 15:36:56 +0100

    Create UserManager implements bundle's UserManagerInterface

---
+++ Entity/Manager/UserManager.php
@@ -0,0 +1,20 @@
+<?php
+/*
+ * This file is part of the Artscore Studio Framework package.
+ *
+ * (c) Nicolas Claverie <info@artscore-studio.fr>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace ASF\UserBundle\Entity\Manager;
+
+use ASF\CoreBundle\Entity\Manager\ASFEntityManager;
+
+/**
+ * User Entity Manager
+ * 
+ * @author Nicolas Claverie <info@artscore-studio.fr>
+ *
+ */
+class UserManager extends ASFEntityManager implements UserManagerInterface {}
